d018bd52d5b00db288e5568cad5e2d208fa1c841
Akshaya Balaji <37283561+akshayavb99@users.noreply.github.com>
2020-06-14 06:33:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 <h1> Testing code </h1>
 <?php
-  $cloud_url='head -n1 path.txt';
+  $cloud_url=`head -n1 path.txt`;
   $img_path="http://".$cloud_url."/Brihadeeshwarar_temple_full.jpg";
   echo "<br>";
   echo "<img src='${img_path}' width=400 height=300>"
